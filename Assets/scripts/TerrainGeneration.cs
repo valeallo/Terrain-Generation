fae5e2f419eab6cb4bce85692d59b5b4f447cd75
valentina.allodi <valentina.allodi@gmail.com>
2022-10-16 11:18:08 +0200

    coloring started (needs fixing)

--- Assets/scripts/TerrainGeneration.cs
+++ Assets/scripts/TerrainGeneration.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
 public class TerrainGeneration : MonoBehaviour
 {
     [SerializeField] private Terrain terrain;
+    [SerializeField] private List<GameObject> tree_prefabs = new List<GameObject>();
+    private List<Vector2Int> mountain_peaks = new List<Vector2Int>();
     private int terrain_resolution;
     public float base_terrain_height = 0.05f;
     public float frequency = 4f;
@@ -16,6 +19,7 @@ public class TerrainGeneration : MonoBehaviour
     public float mountain_amplitude = 0.5f;
     public int base_terrain_octaves = 8;
     public int mountain_octaves = 16;
+    
 
     private void Awake()
     {
@@ -59,6 +63,8 @@ public class TerrainGeneration : MonoBehaviour
         }
         terrain.terrainData.SetHeights(0, 0, heights);
         GenerateMountains(heights);
+        PlaceTrees();
+        PaintTerrain();
     }
 
     
@@ -77,7 +83,7 @@ public class TerrainGeneration : MonoBehaviour
 
     private void GenerateMountains(float[,] heights)
     {
-        List<Vector2Int> mountain_peaks = new List<Vector2Int>();
+        mountain_peaks.Clear();
         while(mountain_peaks.Count < 3)
         {
             float highest_point = 0;
@@ -141,4 +147,82 @@ public class TerrainGeneration : MonoBehaviour
 
     }
     
+    private void PlaceTrees()
+    {
+        GameObject tree_parent = new GameObject("trees");
+        List<Vector2Int> positions = new List<Vector2Int>();
+        //for (int y = 0; y < terrain.terrainData.heightmapResolution; y += 20)
+        //{
+        //    for (int x = 0; x < terrain.terrainData.heightmapResolution; x += 20)
+        //    {
+        //        positions.Add(new Vector2Int(x, y) * 1000/513);
+        //    }
+        //}
+
+        for (int y = 0; y < terrain.terrainData.heightmapResolution; y += 50)
+        {
+            for (int x = 0; x < terrain.terrainData.heightmapResolution; x += 50)
+            {
+                positions.Add(new Vector2Int(x, y));
+            }
+        }
+        foreach (var p in positions) 
+        {
+            float height = terrain.terrainData.GetHeight(p.x, p.y) + terrain.transform.position.y;
+            
+            Debug.DrawLine(new Vector3(p.x,height, p.y), new Vector3(p.x, 0, p.y), Color.red, 1000);
+            foreach (var peak in mountain_peaks)
+            {
+                if (Vector2.Distance(p, peak) > mountain_resolution)
+                {
+                    GameObject tree = Instantiate(tree_prefabs[Random.Range(0, tree_prefabs.Count)], new Vector3(p.x, height, p.y), Quaternion.identity, tree_parent.transform);
+                }
+
+            }
+
+                
+        }
+
+    }
+    private void PaintTerrain() 
+    {
+        TerrainData terrain = GetComponent<Terrain>().terrainData;
+        float[,,] splat_map_data = new float[terrain.alphamapWidth, terrain.alphamapHeight, terrain.alphamapLayers];
+        for (int y = 0; y < terrain.alphamapHeight; y++) { 
+            for(int x = 0; x < terrain.alphamapWidth; x++)
+            {
+                float x_normalized = (float)x / (float)terrain.alphamapWidth;
+                float y_normalized = (float)y / (float)terrain.alphamapHeight;
+                float height = terrain.GetHeight(Mathf.RoundToInt(x_normalized * terrain.heightmapResolution),Mathf.RoundToInt(y_normalized * terrain.heightmapResolution));
+                float steepness = terrain.GetSteepness(x_normalized, y_normalized);
+                height /= terrain.heightmapResolution;
+                float[] splat_weights = new float[terrain.alphamapLayers];
+                splat_weights[0] = 0.2f;
+                splat_weights[2] = height;
+                if (height > 1)
+                {
+                    Debug.Log("error height: " + height);
+                }
+                if (steepness < 45)
+                {
+                    splat_weights[1] = 1 - steepness / 90;
+                }
+                else
+                {
+                    splat_weights[1] = 0.5f - steepness / 90;
+                }
+                float z = splat_weights.Sum();
+                for (int i = 0; i < terrain.alphamapLayers; i++)
+                {
+                    splat_weights[i] /= z;
+                    splat_map_data[x, y, i] = splat_weights[i];
+                }
+            }
+        
+        }
+        terrain.SetAlphamaps(0, 0, splat_map_data);
+
+    
+    
+    }
 }
